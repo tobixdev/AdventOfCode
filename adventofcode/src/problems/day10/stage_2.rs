0090fb304eb2d33acca5901d11b178fc3b24b595
tobixdev <tobias.schwarzinger@gmail.com>
2020-12-10 18:49:06 +0100

    Day10

--- adventofcode/src/problems/day10/stage_2.rs
+++ adventofcode/src/problems/day10/stage_2.rs
@@ -1,26 +1,65 @@
 
-use crate::util::read_lines;
+use crate::util::{powerset, read_lines};
 
 pub fn run() {
     let mut adapters: Vec<u32> = read_lines("./src/problems/day10/input.txt").unwrap().map(|line| line.unwrap().parse::<u32>().unwrap()).collect();
     adapters.sort();
-    let mut last_adapter = 0;
-    let mut arrangements: u64 = 1;
-    for i in 0..adapters.len() {
-        let mut candidates = 1;
-        if i < adapters.len() - 1 && adapters[i + 1] - last_adapter <= 3 {
-            candidates += 1;
-        } else if i < adapters.len() - 2 && adapters[i + 2] - last_adapter <= 3 {
-            candidates += 1;
+    
+    // We have to locate the required numbers (e.g. 4 -> (7) -> 10). These numbers split the problem into multiple independent problems. The smaller
+    // independent problems can be solved using a brute force method. Then multiply the solutions of the subproblems to get the solution to the
+    // actual problem.
+    let mut chunks:Vec<Vec<u32>> = Vec::new();
+    let mut chunk = Vec::new();
+    chunk.push(0);
+    for i in 0..(adapters.len()-1)  {
+        if adapters[i + 1] - chunk[chunk.len() - 1] > 3 {
+            chunk.push(adapters[i]);
+            chunks.push(chunk);
+            chunk = Vec::new();
+            chunk.push(adapters[i]);
+        } else {
+            chunk.push(adapters[i]);
         }
-        let possible_arrangements = match candidates {
-            1 => 1,
-            2 => 3,
-            3 => 6,
-            _ => unreachable!()
-        };
-        arrangements *= possible_arrangements;
-        last_adapter = adapters[i];
     }
+    let last_adapter = adapters[adapters.len() - 1];
+    chunk.push(last_adapter);
+    chunks.push(chunk);
+
+    let mut arrangements: u64 = 1;
+    for mut chunk in chunks {
+        print!("{:?}", chunk);
+        let chunk_arrangements = count_chunk_arrangements(&mut chunk);
+        println!(" ({})", chunk_arrangements);
+        arrangements *= chunk_arrangements;
+    }
+
     println!("Possible arrangements: {}", arrangements);
+}
+
+fn count_chunk_arrangements(chunk: &mut Vec<u32>) -> u64 {
+    let mut chunk_arrangements = 0;
+    let fixed_lower = chunk[0];
+    let fixed_upper = chunk[chunk.len() - 1];
+    chunk.remove(0);
+    chunk.remove(chunk.len() - 1);
+    for mut candidate in powerset(&chunk) {
+        candidate.insert(0, fixed_lower);
+        candidate.push(fixed_upper);
+        if is_valid(&candidate) {
+            chunk_arrangements += 1;
+        }
+    }
+    chunk_arrangements
+}
+
+fn is_valid(chunk: &Vec<u32>) -> bool {
+    let mut last_elem = chunk[0];
+    for elem in chunk {
+        if elem - last_elem > 3 {
+            return false;
+        }
+        last_elem = *elem;
+    }
+
+    true
 }
--- adventofcode/src/util.rs
+++ adventofcode/src/util.rs
@@ -4,3 +4,25 @@ pub fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>> 
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
+
+// Thanks to https://gist.github.com/synecdoche/9ade913c891dda6fcf1cdac823e7d524
+pub fn powerset<T: Clone>(slice: &[T]) -> Vec<Vec<T>> {
+    let mut v: Vec<Vec<T>> = Vec::new();
+
+    for mask in 0..(1 << slice.len()) {
+        let mut ss: Vec<T> = vec![];
+        let mut bitset = mask;
+        while bitset > 0 {
+            // isolate the rightmost bit to select one item
+            let rightmost: u64 = bitset & !(bitset - 1);
+            // turn the isolated bit into an array index
+            let idx = rightmost.trailing_zeros();
+            let item = (*slice.get(idx as usize).unwrap()).clone();
+            ss.push(item);
+            // zero the trailing bit
+            bitset &= bitset - 1;
+        }
+        v.push(ss);
+    }
+    v
+}
